8b75883f2504f33bb3cf363ee7f1ae32606bcca6
leodup <87025285+leodup@users.noreply.github.com>
2022-04-29 14:39:32 -0400

    Add files via upload

---
+++ fine_tuning_utils_mt5.py
@@ -0,0 +1,30 @@
+def compute_metrics(eval_pred):
+    """Computes accuracy, f1, precision, and recall from a 
+    transformers.trainer_utils.EvalPrediction object.
+    """
+    from sklearn import metrics
+    labels = eval_pred.label_ids
+    preds = eval_pred.predictions.argmax(-1)
+
+    accuracy = metrics.accuracy_score(y_true=labels, y_pred=preds)
+    precision, recall, f1, _ = metrics.precision_recall_fscore_support(y_true=labels, y_pred=preds, average=None)
+
+    result = {
+        'accuracy': accuracy,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+    return result
+
+def model_init():
+    """Returns an initialized model for use in a Hugging Face Trainer."""
+    from transformers import MT5Config, MT5Model
+
+    configuration = MT5Config()
+    model = MT5Model(configuration).from_pretrained("mt5-base", num_labels=3)
+
+    return model
+
+
---
+++ train_mt5.py
@@ -0,0 +1,87 @@
+import argparse
+import tg_data
+import data_utils
+import finetuning_utils_mt5
+import pandas as pd
+import torch
+
+
+from transformers import MT5Tokenizer, Trainer, TrainingArguments
+#from ray.tune.suggest.bayesopt import BayesOptSearch
+
+torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser(
+    description="Finetune a MT5 model on the TG dataset."
+)
+parser.add_argument(
+    "data_dir",
+    type=str,
+    help="Directory containing the TG dataset. Can be downloaded from https://github.com/yarakyrychenko/tg-misinfo-data.",
+)
+
+args = parser.parse_args()
+
+train_df = pd.read_csv(f"{args.data_dir}/train.csv")
+val_df = pd.read_csv(f"{args.data_dir}/val.csv")
+test_df = pd.read_csv(f"{args.data_dir}/test.csv")
+
+
+tokenizer = T5Tokenizer.from_pretrained("mt5-bert")
+train_data = tg_data.TGDataset(train_df, tokenizer)
+val_data = tg_data.TGDataset(val_df, tokenizer)
+test_data = tg_data.TGDataset(test_df, tokenizer)
+
+
+trainingargs = TrainingArguments(
+    output_dir='out',
+    do_train=True,
+    do_eval=True,
+    disable_tqdm=False,
+    learning_rate=1e-5,
+    num_train_epochs=1,
+    weight_decay=0.01,
+    logging_steps=500,
+    logging_first_step=True,
+    save_steps=1000,
+    evaluation_strategy = "epoch"
+    )
+
+
+def hp_space(trial):
+    from ray import tune
+    space = {
+        "learning_rate": tune.loguniform(1e-5, 5e-5),
+    }
+    return space
+
+trainer = Trainer(
+    args = trainingargs,
+    tokenizer = tokenizer,
+    train_dataset = train_data,
+    eval_dataset = val_data,
+    model_init = finetuning_utils_mt5.model_init,
+    compute_metrics = finetuning_utils_mt5.compute_metrics,
+    )
+
+print("START")
+
+#best = trainer.hyperparameter_search(
+#    hp_space = hp_space,
+#    direction= "minimize",
+#    backend="ray",
+#    n_trials= 5,
+#    search_alg = BayesOptSearch(metric="objective", mode="min"),
+#    compute_objective=lambda obj : obj['eval_loss']
+#)
+#print(best)
+
+
+print("STARTED TRAINING")
+trainer.train()
+print("TRAINING DONE")
+
+trainer.evaluate()
+
+trainer.save_model()
+print("done done")                      
